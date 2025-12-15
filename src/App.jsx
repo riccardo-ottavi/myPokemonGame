27e8dd110e2adb334c118bfd704642002e338a4f
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-15 15:07:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Display from './components/Display'
+import Team from './components/Team'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <Display />
+      <Team />
+      <Footer />
+    </div> 
+    </>
+  )
+}
+
+export default App
